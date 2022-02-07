97564467e72aa524122dcd77c127657d889cfb46
Julio Cesar Garcia Guillen <garguidevsw@gmail.com>
2022-02-07 05:49:44 -0600

    Dashboard Vue Tailwind - Sidebar

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex w-full min-h-screen font-sans bg-gray-800">
+    <Sidebar />
+  </div>
+</template>
+
+<script setup>
+  import Sidebar from './components/Sidebar.vue'
+</script>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Barlow', 'sans-serif']
+      },
+      colors: {
+        gray: {
+          100: '#E8E6E9',
+          500: '#ABBBC2',
+          700: '#393C49',
+          800: '#252836',
+          900: '#1F1D28',
+        },
+        primary : '#EB966A',
+        accent : {
+          blue: '#65B0F6',
+          orange: '#FFB572',
+          red: '#FF7CA3',
+          green: '#50D1AA',
+          purple : '#9290FE'
+        }
+      },
+      boxShadow : {
+        primary : '0px 8px 24px rgba(234, 124, 105, 0.32)',
+        'inverse-top' : '4px 4px 0 #252836',
+        'inverse-bottom' : '4px -4px 0 #252836'
+      }
+    },
+  },
+  plugins: [],
+}
